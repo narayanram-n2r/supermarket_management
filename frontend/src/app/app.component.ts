95da8b42ad87abc36f1e32974f1affa7f623b435
iprasadbabu112 <prasadbabuirrinki@gmail.com>
2022-07-15 11:29:46 +0530

    First commit by Prasad

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit{
+  title = 'Supermarket';
+  login:string="not success";
+
+  ngOnInit() {
+    this.checkValidation(this.login)
+  }
+
+  checkValidation(result:string){
+    this.login=result;
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClothingComponent } from './Components/Products/clothing/clothing.component';
+import { ElectronicsComponent } from './Components/Products/electronics/electronics.component';
+import { GroceryComponent } from './Components/Products/grocery/grocery.component';
+import { FurnitureComponent } from './Components/Products/furniture/furniture.component';
+import { CartComponent } from './Components/cart/cart.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { HomepageComponent } from './Components/homepage/homepage.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClothingComponent,
+    ElectronicsComponent,
+    GroceryComponent,
+    FurnitureComponent,
+    CartComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomepageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
